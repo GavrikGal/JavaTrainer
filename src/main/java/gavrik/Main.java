aa5512fd22c6087fad6d58c0063bdc4a4581fb9e
GavrikGal <gavrik.gal@gmail.com>
2024-08-13 23:34:12 +0300

    Lesson 3 completed

---
+++ src/main/java/gavrik/Main.java
@@ -0,0 +1,28 @@
+package gavrik;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int age = readInput();
+        String result = "";
+
+        result = age >= 18 ? "взрослый" : "подросток";
+
+        System.out.println(result);
+    }
+
+    public static int readInput() {
+        int age = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLine()) {
+            age = Integer.parseInt(scanner.nextLine());
+        }
+        scanner.close();
+
+        return age;
+    }
+
+}

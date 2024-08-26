e102c334cbad5a4af86eec91e5c17abec7fc41c4
GavrikGal <gavrik.gal@gmail.com>
2024-08-26 21:56:51 +0300

    Lesson 6. Task1 completed

---
+++ lesson6/src/main/java/part1/Main1.java
@@ -0,0 +1,41 @@
+package part1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> data = readInput();
+        boolean result = false;
+
+        result = isEvenList(data);
+
+        System.out.println(result);
+    }
+
+    public static boolean isEvenList(List<Integer> data) {
+        for (Integer i : data) {
+            if (i % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> readInput() {
+        List<Integer> data = null;
+
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            Gson gson = new Gson();
+            data = gson.fromJson(input, new TypeToken<List<Integer>>(){}.getType());
+        }
+        scanner.close();
+
+        return data;
+    }
+}

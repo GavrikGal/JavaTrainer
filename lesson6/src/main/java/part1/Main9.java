43c2151d5aecd5a0765f4d16fa6573cfd6b38c0d
GavrikGal <gavrik.gal@gmail.com>
2024-08-26 23:00:47 +0300

    Lesson 6. Task9 completed

---
+++ lesson6/src/main/java/part1/Main9.java
@@ -0,0 +1,46 @@
+package part1;
+
+import com.google.gson.Gson;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main9 {
+    public static void main(String[] args) {
+        List<String> tags = readInput();
+        String result;
+
+        tags = sortUnique(tags);
+
+        result = toString(tags);
+
+        System.out.println(result);
+    }
+
+    // оставляем уникальные элементы и сортируем
+    public static List<String> sortUnique(List<String> data) {
+        Set<String> set = new TreeSet<>(data);
+        List<String> result = new ArrayList<>(set);
+        Collections.sort(result);
+        return result;
+    }
+
+    // соединяем элементы коллекции через разделитель ", "
+    public static String toString(List<String> data) {
+        return String.join(", ", data);
+    }
+
+    public static List<String> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        if (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            Gson gson = new Gson();
+            String[] dataArray = gson.fromJson(input, String[].class);
+            return Arrays.asList(dataArray);
+        }
+        scanner.close();
+        return null;
+    }
+}
